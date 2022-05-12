e2c5fe6f48f74db62804c96545e993a1e2a0504a
Bosco_Acer <bosco.chw@gmail.com>
2022-05-12 07:58:04 -0700

    implement CRUD services for events

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ const p4 = require("./data/4.json");
 const fire = require("./data/fire.json");
 const allTypes = require("./data/all_types.json");
 const cors = require("cors");
-const db = require("./config/database")
+const db = require("./config/database");
+const Event = require("./models/events");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 db.connect(process.env.MONGO_URI);
 
 app.get("/api/v2/pokemon/:id", (req, res) => {
@@ -28,6 +30,49 @@ app.get("/api/v2/type/:type", (req, res) => {
   const { type } = req.params;
   console.log(type);
   res.json(fire);
+});
+
+app.get("/events", async (req, res) => {
+  const allEvents = await Event.find();
+  res.json(allEvents);
+});
+
+app.post("/events", async (req, res) => {
+  console.log(req.body);
+  const { datetime, text } = req.body;
+  try {
+    const newEvent = await Event.create({ datetime, text });
+    res.json(newEvent);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500);
+    res.json(e);
+  }
+});
+
+app.put("/events/:id", async (req, res) => {
+  const { id } = req.params;
+  const { datetime, text, hits } = req.body;
+  const targetEvent = await Event.findOne({ _id: id }).exec();
+  if (!targetEvent) {
+    res.status(404).send({ message: `no event with id ${id}` });
+  }
+
+  targetEvent.datetime = datetime ? datetime : targetEvent.datetime;
+  targetEvent.text = text ? text : targetEvent.text;
+  targetEvent.hits = hits ? hits : targetEvent.hits;
+  try {
+    const updatedEvent = await targetEvent.save();
+    res.json(updatedEvent);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.delete("/events/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await Event.deleteOne({ _id: id });
+  res.json(result);
 });
 
 app.listen(3000, () => console.log("Server listening on port 3000."));
