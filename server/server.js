94dfc00986cbb7aae4d6c7cb1779a741e49812d0
Bosco_Acer <bosco.chw@gmail.com>
2022-05-11 20:57:48 -0700

    implement a server to serve mock data from pokeapi.cp

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const p1 = require("./data/1.json");
+const p4 = require("./data/4.json");
+const fire = require("./data/fire.json");
+const allTypes = require("./data/all_types.json");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/v2/pokemon/:id", (req, res) => {
+  const data = [p1, p4];
+  const randomNum = Math.floor(Math.random() * 2);
+  res.json(data[randomNum]);
+});
+
+app.get("/api/v2/pokemon", (req, res) => {
+  const { limit } = req.query;
+  console.log(limit);
+  res.json(allTypes);
+});
+
+app.get("/api/v2/type/:type", (req, res) => {
+  const { type } = req.params;
+  console.log(type);
+  res.json(fire);
+});
+
+app.listen(3000, () => console.log("Server listening on port 3000."));
