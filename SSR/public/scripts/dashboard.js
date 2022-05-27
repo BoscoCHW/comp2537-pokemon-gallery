ed3cd42ca64a37a9ef4b8a98267873197829dc3d
Bosco_Acer <bosco.chw@gmail.com>
2022-05-27 10:33:02 -0700

    finish update user functionality

--- SSR/public/scripts/dashboard.js
+++ SSR/public/scripts/dashboard.js
@@ -24,20 +24,61 @@ userCards.forEach((userCard) => {
   });
 });
 
-// Adds edit button functionalities for each worry card in the daily view page
-document.querySelectorAll(".editBtn").forEach((editBtn) => {
-  editBtn.addEventListener("click", () => {
-    window.location.href = `/editWorryEntry/${editBtn.id}`;
+const modal = document.querySelector("#exampleModal");
+modal.addEventListener("show.bs.modal", function (event) {
+  // Button that triggered the modal
+  const button = event.relatedTarget;
+  if (button.id === "addUserBtn") return;
+  // Extract info from data-bs-* attributes
+  const userId = button.getAttribute("data-bs-userId");
+  const firstname = button.getAttribute("data-bs-firstname");
+  const lastname = button.getAttribute("data-bs-lastname");
+  const email = button.getAttribute("data-bs-email");
+  const password = button.getAttribute("data-bs-password");
+  const isAdmin = button.getAttribute("data-bs-isAdmin") === "true";
+  // console.log(userId, firstname, lastname, email, password, isAdmin);
+
+  // Update the modal's content.
+  const modalTitle = modal.querySelector(".modal-title");
+  modalTitle.textContent = `Edit User ${firstname} ${lastname}`;
+  const inputs = modal.querySelectorAll(".modal-body input");
+  inputs.forEach((input) => {
+    switch (input.id) {
+      case "firstname":
+        input.value = firstname;
+        break;
+      case "lastname":
+        input.value = lastname;
+        break;
+      case "email":
+        input.value = email;
+        break;
+      case "password":
+        input.value = password;
+        break;
+      case "isAdmin":
+        input.checked = isAdmin;
+        break;
+      case "userId":
+        input.value = userId;
+        break;
+    }
   });
 });
 
+// add user
 const userFormSubmitBtn = document.querySelector("#userFormSubmitBtn");
 userFormSubmitBtn.addEventListener("click", async (e) => {
-  const body = new URLSearchParams(
-    new FormData(document.querySelector("#userForm"))
-  );
-
-  const resp = await fetch("/api/user/create", { method: "POST", body });
+  const formData = new FormData(document.querySelector("#userForm"));
+  const userId = formData.get("userId");
+  console.log(userId);
+  const body = new URLSearchParams(formData);
+  let resp;
+  if (userId) {
+    resp = await fetch(`/api/user/update/${userId}`, { method: "PUT", body });
+  } else {
+    resp = await fetch("/api/user/create", { method: "POST", body });
+  }
   if (resp.ok) {
     location.href = "/adminDashboard";
   } else {
