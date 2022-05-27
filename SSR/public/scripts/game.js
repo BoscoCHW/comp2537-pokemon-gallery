7f1d4f23a65a1592371244532e3aa3c903e1bea8
Bosco_Acer <bosco.chw@gmail.com>
2022-05-27 12:56:21 -0700

    implement game config from user inputs

--- SSR/public/scripts/game.js
+++ SSR/public/scripts/game.js
@@ -1,15 +1,65 @@
 import { shuffle } from "./helpers.js";
 
+// game configs
+let width, length, numOfPokemons;
+let numOfCards;
+
+// game logic variables
+let firstCard = null;
+let secondCard = null;
+let pokemonCardsLocked = false;
+let flippedCardNum = 0;
+
+// pokemon image url
 const pokemonImgUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
 
-window.addEventListener("load", (e) => {
-  const numOfPokemons = 8;
+const startBtn = document.querySelector("#start-btn");
+startBtn.addEventListener("click", (e) => {
+  const gameConfigsDiv = e.target.parentElement;
+  const inputs = gameConfigsDiv.querySelectorAll("input");
+  inputs.forEach((input) => {
+    switch (input.id) {
+      case "width":
+        width = Number(input.value);
+        break;
+      case "length":
+        length = Number(input.value);
+        break;
+      case "pokemonNums":
+        numOfPokemons = Number(input.value);
+    }
+  });
+
+  if ((width * length) % 2 !== 0) {
+    const errorDiv = document.querySelector(".error-message");
+    errorDiv.textContent =
+      "Width and length should multiply to an even number.";
+    setTimeout(() => {
+      errorDiv.textContent = "";
+    }, 1000);
+    return;
+  }
+
+  if (numOfPokemons < 2 || numOfPokemons * 2 > width * length) {
+    const errorDiv = document.querySelector(".error-message");
+    errorDiv.textContent = "Invalid number of pokemons.";
+    setTimeout(() => {
+      errorDiv.textContent = "";
+    }, 1000);
+    return;
+  }
+  
+  gameConfigsDiv.parentElement.removeChild(gameConfigsDiv);
+  
   const gameGrid = document.getElementById("game-grid");
+  gameGrid.style.width = `${110 * length + 96}px`
+
+  numOfCards = width * length;
   const pokemonCards = [];
 
   let pokemonId;
-  for (let i = 0; i < numOfPokemons * 2; i++) {
+  for (let i = 0; i < numOfCards; i++) {
     if (i % 2 == 0) {
       pokemonId = Math.floor(Math.random() * 900) + 1;
     }
@@ -17,7 +67,7 @@ window.addEventListener("load", (e) => {
     pokemonCard.classList.add("card");
     pokemonCard.id = `card-${i}`;
     pokemonCard.innerHTML = `
-        <img id="img${i}" class="front_face" src="${pokemonImgUrl}${pokemonId}.png" alt="" />
+        <img class="front_face" src="${pokemonImgUrl}${pokemonId}.png" alt="" />
         <img class="back_face" src="/img/pokeball.png" alt="" />
     `;
     pokemonCards.push(pokemonCard);
@@ -25,54 +75,56 @@ window.addEventListener("load", (e) => {
 
   const shuffledCards = shuffle(pokemonCards);
 
-  shuffledCards.forEach((card) => gameGrid.appendChild(card));
-
-  setup();
+  shuffledCards.forEach((card) => {
+    card.addEventListener("click", handleCardClick);
+    gameGrid.appendChild(card);
+  });
 });
 
-let firstCard = undefined;
-let secondCard = undefined;
+function handleCardClick(event) {
+  if (pokemonCardsLocked) return;
 
-let firstCardHasBeenFlipped = false;
-let pokemonCardsLocked = false;
-
-function setup() {
-  $(".card").on("click", function () {
-    if (pokemonCardsLocked) return;
-
-    $(this).toggleClass("flip");
-
-    if (!firstCardHasBeenFlipped) {
-      // the first card
-      firstCard = $(this).find(".front_face")[0];
-      // console.log(firstCard);
-      firstCardHasBeenFlipped = true;
-    } else {
-      // this is the 2nd card
-      secondCard = $(this).find(".front_face")[0];
-
-      console.log(firstCard, secondCard);
-      // ccheck if we have match!
-      if (
-        firstCard.id !== secondCard.id &&
-        $(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src")
-      ) {
-        console.log("a match!");
-        // update the game state
-        // disable clicking events on these cards
-        $(`#${firstCard.id}`).parent().off("click");
-        $(`#${secondCard.id}`).parent().off("click");
-      } else {
-        console.log("not a match");
-        pokemonCardsLocked = true;
-        // unflipping
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().removeClass("flip");
-          $(`#${secondCard.id}`).parent().removeClass("flip");
-          firstCardHasBeenFlipped = false;
-          pokemonCardsLocked = false;
-        }, 1000);
-      }
+  if (!firstCard) {
+    // the first card
+    firstCard = event.currentTarget;
+    firstCard.classList.toggle("flip");
+  } else {
+    // this is the 2nd card
+    secondCard = event.currentTarget;
+    // if the second card is the same as the first card, do nothing
+    if (firstCard.id === secondCard.id) {
+      secondCard = null;
+      return;
     }
-  });
+    // 2nd card is not the same, flip the card
+    secondCard.classList.toggle("flip");
+    // check if we have match!
+    const firstCardPokemonSrc = firstCard.querySelector(".front_face").src;
+    const secondCardPokemonSrc = secondCard.querySelector(".front_face").src;
+
+    if (firstCardPokemonSrc === secondCardPokemonSrc) {
+      console.log("a match!");
+      // update the game state
+      // disable clicking events on these cards
+      firstCard.removeEventListener("click", handleCardClick);
+      secondCard.removeEventListener("click", handleCardClick);
+      firstCard = null;
+      secondCard = null;
+      flippedCardNum += 2;
+      if (flippedCardNum == 16) {
+        console.log("you won!");
+      }
+    } else {
+      console.log("not a match");
+      pokemonCardsLocked = true;
+      // unflipping
+      setTimeout(() => {
+        firstCard.classList.toggle("flip");
+        secondCard.classList.toggle("flip");
+        firstCard = null;
+        secondCard = null;
+        pokemonCardsLocked = false;
+      }, 1000);
+    }
+  }
 }
